d9f4016ca09d41a04138796490c2a8704358c737
Yuchen Yang <707243772@qq.com>
2020-05-19 01:03:20 +0800

    依然主页部分

--- pages/realPages/mainUi/mainUi.js
+++ pages/realPages/mainUi/mainUi.js
@@ -5,9 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    dates: '2020-05-18',
+    calorie_taken_in: 0,
+    calorie_burned: 0,
+    calorie_potential: 2000,
+    messageOfBreakfast:"未设置",
+    messageOfLunch:"未设置",
+    messageOfDinner:"未设置",
+    messageOfExercise:"未设置",
+    messageOfExture:"未设置"
   },
 
+    //点击日期组件确定事件  
+    bindDateChange: function (e) {
+      console.log(e.detail.value)
+     this.setData({
+       dates: e.detail.value
+     })
+   },
+
+   goToAnalysisPage:function(){
+    wx.navigateTo({
+      url: '../../../pages/realPages/analysis/analysis',
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
