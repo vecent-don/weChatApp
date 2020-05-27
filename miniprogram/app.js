cea9836932cac6f539516dad7ea291e4e07c4a6d
Graktel-kui <531129143@qq.com>
2020-05-27 15:44:33 +0800

    dev：
    完善点击事件，显示添加食物功能，可以跳转详细界面，连接云数据库，但没有完成specific界面数据绑定，界面不够美观

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -5,7 +5,9 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    wx.cloud.init({
+      env:"nju-calorie-n006e"
+    })
     // 登录
     wx.login({
       success: res => {
--- miniprogram/pages/realPages/recipe/recipe.js
+++ miniprogram/pages/realPages/recipe/recipe.js
@@ -1,5 +1,9 @@
+const DataBase_all = wx.cloud.database().collection("all_data")
+const DataBase_userMenu = wx.cloud.database().collection("userMenu")
+var util = require('../../../utils/util.js')
+var app = getApp()
 
-var app = getApp();
+
 Page({
 
   /**
@@ -8,6 +12,13 @@ Page({
   data: {
       navbar:['常见','收藏','自定义'],
       currentTab:0,
+      cloud_list:[],
+      multiArray:[['今天','昨天','前天'],['早餐','中餐','晚餐','加餐']],
+      multiIndex: [0, 0],
+      current_weight:0,
+      food_name:null,
+      food_heat:null,
+      open_id:null,
       food_list:[
         {name:"黄米",
          heat: 351,
@@ -85,9 +96,80 @@ Page({
         },
       ]
   },
+  //负责模态框的时间和早午晚餐选择
+  bindMultiPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndex: e.detail.value
+    })
+  },
   navbarTap: function(e){
     this.setData({
       currentTab: e.currentTarget.dataset.idx
+    })
+  },
+  go_to_specific:function(){
+    wx.navigateTo({
+      url: '/pages/realPages/specific/specific',
+    })
+  },
+  //负责模态框的展示与否
+  showModal:function(e) {
+    console.log(e);
+    this.setData({
+      modalName: e.currentTarget.dataset.target,
+      food_name: e.currentTarget.dataset.name,
+      food_heat: e.currentTarget.dataset.heat,
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
+  addData(){
+    let that = this
+    this.setData({
+      modalName: null
+    })
+    DataBase_userMenu.where({openid:that.data.open_id,date:that.data.current_date}).get({
+      success(res){
+        var list_tmp;
+        if(meal_time == 1){
+          list_tmp = res.data[0].breakfast
+        }
+        else if(meal_time == 2){
+          list_tmp = res.data[0].lunch
+        }
+        else if(meal_time == 3){
+          list_tmp = res.data[0].dinner
+        }
+        else{
+          list_tmp = res.data[0].other
+        }
+        list_tmp.add({heat,food_name,food_weight})
+        console.log(list_tmp)
+      },
+      fail(res){
+        console.log("添加失败",res)
+      }
+    })
+      
+    DataBase_all.add({
+      data:{
+        opid:that.data.open_id,
+        date:that.data.current_date,
+        meal_time:that.data.multiIndex[1]+1,
+        food_name:that.data.food_name,
+        food_heat:that.data.food_heat,
+        weight:that.data.current_weight
+      },
+      success(res){
+        console.log("添加成功",res)
+      },
+      fail(res){
+        console.log("添加失败",res)
+      }
     })
   },
 
@@ -95,8 +177,51 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var time = util.formatDate(new Date());
+    this.setData({
+      current_date:time
+    })
+    let that=this;
+    wx.cloud.callFunction({
+      name:"get_openid",
+      success(res){
+        console.log("获取openid成功")
+        var openid_tmp = res.result.openid
+        that.setData({
+          open_id: res.result.openid
+        })
+        console.log(that.data.open_id)
+      },fail(res){
+        console.log("获取openid失败",res)
+      }
+    })
+    
+    wx.cloud.database().collection("food_list").get({
+      success(res){
+        console.log("请求成功",res);
+        that.setData({
+          cloud_list:res.data
+        })
+      },
+      fail(res){
+        console.log("请求失败",res);
+      }
+    })
   },
+  
+  tapKey:function(event){
+    var x = event.currentTarget.dataset.key
+    this.setData({
+      current_weight:this.data.current_weight=='0'? x:this.data.current_weight + x
+    })
+  },
+  tapDel:function(event){
+    if(this.data.current_weight==0)return 
+    this.setData({
+      current_weight: this.data.current_weight.length == 1? '0': this.data.current_weight.substring(0, this.data.current_weight.length - 1)
+    })
+  },
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
