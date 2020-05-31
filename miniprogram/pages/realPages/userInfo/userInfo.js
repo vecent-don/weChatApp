b0102fb044c1c8d88379405553d50051fac02b8f
vecent-don <181250127@smail.nju.edu.cn>
2020-05-31 11:38:00 +0800

    feat
    user功能正式完成

--- miniprogram/pages/realPages/userInfo/userInfo.js
+++ miniprogram/pages/realPages/userInfo/userInfo.js
@@ -1,139 +1,152 @@
 
 
-// const app = getApp();
-// var util = require('../../../utils/util.js')
-// const DataBase_user = wx.cloud.database().collection("userInfo")
-// Page({
-//   data: {
-//     modalName:null,
-//     nowWeight: 0,
-//     targetWeight: -1,
-//     height:0,
-//     BMI: 0,
-//     motto: 'Hello---- World',
-//     userInfo: {},
-//     hasUserInfo: false,
-//     canIUse: wx.canIUse('button.open-type.getUserInfo')
-//   },
-//   watch:{
+const app = getApp();
+var util = require('../../../utils/util.js')
+const DataBase_user = wx.cloud.database().collection("userInfo")
+Page({
+  data: {
+    modalName:null,
+    nowWeight: 0,
+    targetWeight: -1,
+    height:0,
+    BMI: 0,
+    motto: 'Hello---- World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  watch:{
     
-//   },
-//   bindViewTap: function() {
-//     wx.navigateTo({
-//       url: '../logs/logs'
-//     })
-//   },
-//   onLoad: function () {
-//     if (app.globalData.userInfo) {
-//       this.setData({
-//         userInfo: app.globalData.userInfo,
-//         hasUserInfo: true
-//       })
-//     } else if (this.data.canIUse){
-//       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-//       // 所以此处加入 callback 以防止这种情况
-//       app.userInfoReadyCallback = res => {
-//         this.setData({
-//           userInfo: res.userInfo,
-//           hasUserInfo: true
-//         })
-//       }
-//     } else {
-//       // 在没有 open-type=getUserInfo 版本的兼容处理
-//       wx.getUserInfo({
-//         success: res => {
-//           app.globalData.userInfo = res.userInfo
-//           this.setData({
-//             userInfo: res.userInfo,
-//             hasUserInfo: true
-//           })
-//         }
-//       })
-//     }
-//     var temp={}
+  },
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    var temp={}
    
-//      wx.cloud.database().collection('userInfo').where({openid:app.globalData.ids.openid}).get()
-//     .then(res=>{
-//       temp=res.data[0];
+     wx.cloud.database().collection('userInfo').where({openid:app.globalData.ids.openid}).get()
+    .then(res=>{
+      temp=res.data[0];
       
-//       this.setData({
-//         'nowWeight':temp.nowWeight,
-//         'targetWeight':temp.targetWeight,
-//         'BMI':(temp.nowWeight/(temp.height*temp.height/10000)).toFixed(2)
-//       })
+      this.setData({
+        'nowWeight':temp.nowWeight,
+        'targetWeight':temp.targetWeight,
+        'BMI':(temp.nowWeight/(temp.height*temp.height/10000)).toFixed(2)
+      })
    
-//       console.log(this.BMI)
-//     }).catch(err=>{console.log(err)})
-//     ;
+      console.log(this.BMI)
+    }).catch(err=>{console.log(err)})
+    ;
    
-//   },
+  },
+   onReady: function () {
+    wx.cloud.database().collection('userInfo').where({openid:app.globalData.ids.openid}).get()
+    .then(res=>{
+      temp=res.data[0];
+      
+      this.setData({
+        'nowWeight':temp.nowWeight,
+        'targetWeight':temp.targetWeight,
+        'BMI':(temp.nowWeight/(temp.height*temp.height/10000)).toFixed(2)
+      })
+   
+      console.log(this.BMI)
+    }).catch(err=>{console.log(err)})
+  },
+
+  getUserInfo: function(e) {
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  showModal:function(e) {
+    console.log(e);
+    this.setData({
+      modalName: e.currentTarget.dataset.target,
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
   
+  getInput_nowWeight:function(e){
+    this.setData({
+      nowWeight: e.detail.value
+    })
+},
+  getInput_targetWeight:function(e){
+  this.setData({
+    targetWeight: e.detail.value
+  })
+},
+  getInput_height:function(e){
+  this.setData({
+    height: e.detail.value
+  })
+},
+  addData(){
+    let that = this
+    this.setData({
+      modalName: null,
+      BMI:(this.data.nowWeight/(this.data.height*this.data.height/10000)).toFixed(2)
+    })
+ wx.cloud.callFunction({
+  name:'update_Info',
+  data:{
+    'nowWeight':that.data.nowWeight,
+    'targetWeight': that.data.targetWeight,
+    'height':that.data.height
+  },
+}).then(res=>{
+  console.log("dsd")
+console.log(res)
+});
 
-//   getUserInfo: function(e) {
-//     app.globalData.userInfo = e.detail.userInfo
-//     this.setData({
-//       userInfo: e.detail.userInfo,
-//       hasUserInfo: true
-//     })
-//   },
-
-//   showModal:function(e) {
-//     console.log(e);
-//     this.setData({
-//       modalName: e.currentTarget.dataset.target,
-//     })
-//   },
-//   hideModal(e) {
-//     this.setData({
-//       modalName: null
-//     })
-//   },
-  
-//   getInput_nowWeight:function(e){
-//     this.setData({
-//       nowWeight: e.detail.value
-//     })
-// },
-//   getInput_targetWeight:function(e){
-//   this.setData({
-//     targetWeight: e.detail.value
-//   })
-// },
-//   getInput_height:function(e){
-//   this.setData({
-//     height: e.detail.value
-//   })
-// },
-//   addData(){
-//     let that = this
-//     this.setData({
-//       modalName: null,
-//       BMI:(this.data.nowWeight/(this.data.height*this.data.height/10000)).toFixed(2)
-//     })
-//  wx.cloud.callFunction({
-//   name:'update_Info',
-//   data:{
-//     'nowWeight':that.data.nowWeight,
-//     'targetWeight': that.data.targetWeight,
-//     'height':that.data.height
-//   },
-// }).then(res=>{
-//   console.log("dsd")
-// console.log(res)
-// });
-
-// // 'date':util.formatDate(new Date()),
-//   wx.cloud.callFunction({
-//   name:'update_TimeLine',
-//   data:{
+// 'date':util.formatDate(new Date()),
+  wx.cloud.callFunction({
+  name:'update_TimeLine',
+  data:{
     
-//     "nowWeight": 50,
-//   "date": "2020-6-20"    
-//   },
-//   }).then(res=>{
-//     console.log("sd")
-//     console.log(res)
-//   });
+    "nowWeight": that.data.nowWeight,
+  "date": util.formatDate(new Date()),    
+  },
+  }).then(res=>{
+    console.log("sd")
+    console.log(res)
+  });
 
-//   },
-// });
+  },
+});
